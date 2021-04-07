1d40f6bc4a70652d4908511932ce7bc3609a4382
sueperb <david@tusks.org.uk>
2021-04-07 14:35:42 +0100

    Tidied up coed and added logging

--- kahoot.py
+++ kahoot.py
@@ -3,61 +3,89 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 import time
 import random
+import sys
+import logging
+
+"""
+Bot class for connecting to kahoot.
+"""
 
 class Bot:
+    """
+    Bot class to connect to kahoot.
+    run() will:
+        Make a selenium headless browser instance
+        Input pin and nickname
+        Loop checking for buttons to press
+        Pick a random answer button and press
+        Check for game end
+        If the game is ended then will exit
+    """
 
     def __init__(self):
-        self.auto_names = [""]
+        pass
 
     def run(self, game_pin, nickname):
+        """Run the bot. Connects the bot to the game pin specified, with the nickname specified."""
 
-        #set selenium options
+        #set selenium options to headless mode, then connect to browser instance
         opts = Options()
-
-        #set to be headless
         opts.set_headless()
         assert opts.headless
+        self.browser = Firefox(options=opts)
 
-        #make a browser instance
-        browser = Firefox(options=opts)
+        #join the game with pin and nickname
+        self.join_game(game_pin, nickname)
 
-        #load kahoot page
-        browser.get("https://kahoot.it")
+        logging.info(f"Connected bot with name '{nickname}' to game with pin {game_pin}")
 
-        #find pin input and submit pin
-        input_form = browser.find_element_by_id('game-input')
-        input_form.send_keys(game_pin)
-        input_form.submit()
-
-        #wait, so that the page is loaded
-        browser.implicitly_wait(10)
-
-        #find nickname input and submit nickname
-        nickname_form = browser.find_element_by_id("nickname")
-        nickname_form.send_keys(nickname)
-        nickname_form.submit()
-
-        browser.implicitly_wait(10)
+        #wait for page to load
+        self.browser.implicitly_wait(10)
 
         #wait for game to start, then click random button. repeat
         while True:
             while True:
-                print(browser.current_url)
                 try:
-                    #if browser.current_url == "https://kahoot.it/v2/ranking" or browser.current_url == "https://kahoot.it/v2/answer/result":
-                    #    browser.close()
-                    buttons = browser.find_elements_by_tag_name("button")
+                    self.check_end()
+                    buttons = self.browser.find_elements_by_tag_name("button")
                     if len(buttons) < 4:
                         continue
                     break
                 except selenium.common.exceptions.NoSuchElementException:
-                    #if browser.current_url == "https://kahoot.it/v2/ranking" or browser.current_url == "https://kahoot.it/v2/answer/result":
-                    #    browser.close()
+                    self.check_end()
                     continue
+            logging.info("Answering question with random answer")
+            self.random_answer(buttons)
 
-            answer = random.choice(buttons)
-            time.sleep(random.choice([0.75, 1, 2]))
-            answer.click()
+    def join_game(self, pin, nick):
+        """Join game specified"""
+        self.browser.get("https://kahoot.it")
+        self.input_pin(pin)
+        #wait for page to load
+        self.browser.implicitly_wait(10)
+        self.input_nick(nick)
 
-    def auto(self, num):
-        pass
+    def input_pin(self, pin):
+        """Find pin input and submit"""
+        input_form = self.browser.find_element_by_id('game-input')
+        input_form.send_keys(pin)
+        input_form.submit()
+
+    def input_nick(self, nick):
+        """Find nickname input and submit"""
+        nickname_form = self.browser.find_element_by_id("nickname")
+        nickname_form.send_keys(nick)
+        nickname_form.submit()
+
+    def random_answer(self, buttons):
+        """Pick a random answer from the answer buttons, wait for a random time (for human feel) then click"""
+        answer = random.choice(buttons)
+        time.sleep(random.choice([0.75, 1, 2]))
+        answer.click()
+
+    def check_end(self):
+        """Exits the program if the game has ended."""
+        if self.browser.current_url == "https://kahoot.it/v2/ranking":
+            self.browser.close()
+            logging.info("Game ended, exiting...")
+            sys.exit()
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import kahoot
+import sys
+import logging
+
+"""
+File to get input, then make a Bot instance and run.
+"""
 
 def main(game_pin, nickname):
-
+    """Make an instance and run"""
     bot = kahoot.Bot()
+    try:
+        bot.run(game_pin, nickname)
+    except ValueError:
+        print("Your game pin was not valid.")
+        sys.exit()
 
-    bot.run(game_pin, nickname)
+def get_input():
+    """Get game pin and nickname"""
+    pin = input("Input game pin: ")
+    try:
+        pin = int(pin)
+    except ValueError:
+        print("Game pin must be an integer.")
+        sys.exit()
+    nick = input("Input nickname: ")
+    return pin, nick
 
 if __name__ == "__main__":
-    game_pin = input("Please provide the game pin: ")
-    nickname = input("Please provide the bot's nickname: ")
-    main(game_pin, nickname)
+    logging.basicConfig(level=logging.INFO)
+    pin, nick = get_input()
+    main(pin, nick)
